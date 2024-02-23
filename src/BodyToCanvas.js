17e14ddc6e86e4cf6ca6990f1ccbfcb2698b4885
Barbara Kälin <67426447+BarbWire-1@users.noreply.github.com>
2024-02-23 18:52:17 +0100

    formatted?

--- src/BodyToCanvas.js
+++ src/BodyToCanvas.js
@@ -5,19 +5,17 @@ export class BodyToCanvas {
 		this.offscreenCtx = this.offscreenCanvas.getContext('2d', {
 			willReadFrequently: true,
 		});
-		
 	}
 
 	renderBodyToCanvas() {
 		if (this.isRendering) return;
-        this.isRendering = true;
+		this.isRendering = true;
 
 		html2canvas(document.body, {
 			backgroundColor: 'transparent',
 			scale: 1,
 			logging: false, // Disable logging
-        }).then((bodyCanvas) => {
-
+		}).then((bodyCanvas) => {
 			const { width, height } = bodyCanvas;
 			this.offscreenCanvas.width = width;
 			this.offscreenCanvas.height = height;
